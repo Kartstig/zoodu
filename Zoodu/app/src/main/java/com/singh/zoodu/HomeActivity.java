878164745c053bcf706a3697c3c4792c20b0d9bd
Herman Singh <kartstig@gmail.com>
2016-01-29 15:40:01 -0500

    Working list view with bluetooth signal output

--- Zoodu/app/src/main/java/com/singh/zoodu/HomeActivity.java
+++ Zoodu/app/src/main/java/com/singh/zoodu/HomeActivity.java
@@ -4,19 +4,16 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.Set;
-import java.util.logging.Handler;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
     // Debugging
@@ -27,12 +24,15 @@ public class HomeActivity extends AppCompatActivity {
     private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     private ArrayAdapter<String> deviceArrayAdapter;
     private ListView deviceListView;
+    private boolean isEnabled;
+    private boolean isScanning;
+    private HashMap<String, String> deviceHash = new HashMap<String, String>();
+    private static final long SCAN_PERIOD = 10000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if(D) {
-            Log.i(TAG, "+++ ON CREATE +++");
-        }
+        if(D) { Log.i(TAG, "+++ ON CREATE +++"); }
         super.onCreate(savedInstanceState);
 
         initializeGUI();
@@ -44,15 +44,6 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         deviceArrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1);
         deviceListView = (ListView) findViewById(R.id.deviceView);
@@ -61,35 +52,42 @@ public class HomeActivity extends AppCompatActivity {
 
     public void checkBluetooth() {
         if (bluetoothAdapter == null) {
-            Log.e(TAG, "ERR: No Bluetooth Adapter");
+            if (D) { Log.e(TAG, "ERR: No Bluetooth Adapter"); }
+            deviceArrayAdapter.add("Error. No Bluetooth Adapter present.");
+            finish();
+        } else if (!bluetoothAdapter.isEnabled()) {
+            if (D) { Log.d(TAG, "Enabling Bluetooth Adapter"); }
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
         } else {
-            Log.d(TAG, "Found Bluetooth Adapter");
-            if (!bluetoothAdapter.isEnabled()) {
-                Log.d(TAG, "Enabling Bluetooth Adapter");
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, 1);
-            } else {
-                Log.d(TAG, "Bluetooth Already Enabled");
-                getDeviceList();
-            }
+            isEnabled = true;
+            if (D) { Log.d(TAG, "Bluetooth Already Enabled"); }
+            scanLeDevices(true);
         }
     }
 
-
-    private boolean isScanning;
-    private Handler btHandler;
-    private void getDeviceList() {
-        Set<BluetoothDevice> nearbyDevices = bluetoothAdapter.getBondedDevices();
-        // If there are paired devices
-        if (nearbyDevices.size() > 0) {
-            // Loop through paired devices
-            for (BluetoothDevice device : nearbyDevices) {
-                // Add the name and address to an array adapter to show in a ListView
-                deviceArrayAdapter.add(device.getName() + "\n" + device.getAddress());
-            }
+    private void scanLeDevices(final boolean enable) {
+        if (enable && !isScanning) {
+            bluetoothAdapter.startLeScan(leDeviceCallback);
+            isScanning = true;
         } else {
-            Log.e(TAG, "No Bluetooth Devices found");
-            deviceArrayAdapter.add("No devices found");
+            bluetoothAdapter.stopLeScan(leDeviceCallback);
+            isScanning = false;
+        }
+    }
+
+    private BluetoothAdapter.LeScanCallback leDeviceCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+            deviceHash.put(device.getAddress(), device.getName() + "\n" + "RSSI: " + rssi + " dBm");
+            rebuildArray();
+        }
+    };
+
+    private void rebuildArray() {
+        deviceArrayAdapter.clear();
+        for (Map.Entry<String, String> entry : deviceHash.entrySet()) {
+            deviceArrayAdapter.add(entry.getValue());
         }
     }
 
@@ -117,18 +115,17 @@ public class HomeActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(resultCode == RESULT_OK) {
-            Log.d(TAG, "Bluetooth Enabled");
-            getDeviceList();
+            if (D) { Log.d(TAG, "Bluetooth Enabled"); }
+            scanLeDevices(true);
         } else {
-            Log.e(TAG, "Failed turning on Bluetooth");
+            if (D) { Log.e(TAG, "Failed turning on Bluetooth"); }
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(D) {
-            Log.e(TAG, "--- ON DESTROY ---");
-        }
+
+        if(D) { Log.e(TAG, "--- ON DESTROY ---"); }
     }
 }
